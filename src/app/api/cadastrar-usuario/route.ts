b610eaf903c08ce54c76bd2a3da63fbc41dff3cc
ronycode <ronypc@outlook.com>
2023-12-19 21:50:17 -0300

    page cadastro

--- src/app/api/cadastrar-usuario/route.ts
+++ src/app/api/cadastrar-usuario/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     });
   }
 
-  const res = await fetch(`${process.env.API_GSO}/api/auth/cadastro/usuario`, {
+  const res = await fetch(`${process.env.API_GSO}/api/auth/cadastro`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
